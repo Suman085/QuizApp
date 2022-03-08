9cc0b81456f0c2272bc66ead8d972e5c4d0ced61
suman maharjan <suman.maharjan@joinsage.com>
2022-03-09 00:52:12 +0545

    chore: change port:

--- client/src/consts/config.ts
+++ client/src/consts/config.ts
@@ -1,2 +1,2 @@
-export const SOCKET_URL = "https://sleepy-journey-01693.herokuapp.com";
+export const SOCKET_URL = "https://sleepy-journey-01693.herokuapp.com:26852";
 // export const SOCKET_URL = "http://localhost:8000";
